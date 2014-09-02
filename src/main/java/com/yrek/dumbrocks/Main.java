00452dc4ffc7f6356f77f8ed3f4f1e214c0195a8
Q.P.Liu <qpliu@yahoo.com>
2014-09-01 18:14:26 -0700

    Update for library changes.

--- src/main/java/com/yrek/dumbrocks/Main.java
+++ src/main/java/com/yrek/dumbrocks/Main.java
@@ -17,36 +17,43 @@ public class Main {
         int width = 80;
         int height = 24;
         DumbGlk glk = new DumbGlk(new File(new File(System.getProperty("user.home", "/tmp"), ".dumbGlk"), storyFile.getName()), new InputStreamReader(System.in), new OutputStreamWriter(System.out), width, height);
+        ZCode zcode = null;
+        Glulx glulx = null;
         try {
             Blorb blorb = Blorb.from(storyFile);
             for (Blorb.Resource res : blorb.resources()) {
                 if (res.getUsage() == Blorb.Exec && res.getChunk().getId() == Blorb.GLUL) {
-                    new Glulx(res.getChunk().getContents(), glk).run();
-                    return;
+                    glulx = new Glulx(res.getChunk().getContents(), glk);
                 }
                 if (res.getUsage() == Blorb.Exec && res.getChunk().getId() == Blorb.ZCOD) {
-                    new ZCode(res.getChunk().getContents(), new GlkDispatch(glk)).run();
-                    return;
+                    zcode = new ZCode(res.getChunk().getContents(), new GlkDispatch(glk)).initGlk(0,0);
                 }
             }
         } catch (Exception e) {
         }
-        FileInputStream fin = new FileInputStream(storyFile);
-        int magic;
-        try {
-            magic = fin.read();
-        } finally {
-            fin.close();
+        if (zcode == null && glulx == null) {
+            FileInputStream fin = new FileInputStream(storyFile);
+            int magic;
+            try {
+                magic = fin.read();
+            } finally {
+                fin.close();
+            }
+            switch (magic) {
+            case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8:
+                zcode = new ZCode(storyFile, new GlkDispatch(glk)).initGlk(0,0);
+                break;
+            case 71:
+                glulx = new Glulx(storyFile, glk);
+                break;
+            default:
+                break;
+            }
         }
-        switch (magic) {
-        case 1: case 2: case 3: case 4: case 5: case 6: case 7: case 8:
-            new ZCode(storyFile, new GlkDispatch(glk)).run();
-            return;
-        case 71:
-            new Glulx(storyFile, glk).run();
-            return;
-        default:
-            break;
+        if (zcode != null) {
+            zcode.run();
+        } else if (glulx != null) {
+            glulx.run();
         }
     }
 }
